8e2882f947335c7812b9a6f5cadfcae39c59bc2b
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-01-06 14:35:58 +0900

    Add htmlTemplatePlugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const MyWebPackPlugin = require('./my-webpack-plugin')
 const path = require('path')
 const webpack = require('webpack')
 const childProcess = require('child_process')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // node.js의 모듈 시스템
 module.exports = {
@@ -24,7 +25,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'url-loader',
         options: {
-          publicPath: './dist', // prefix를 아웃풋 경로로 지정한다.
+          //publicPath: './dist', // prefix를 아웃풋 경로로 지정한다. -> src 밑으로 index.html 을 옮겼기 때문에 사용하지 않아도 된다.
           name: '[name].[ext]?[hash]', // 파일명 형식
           limit: 300000, // url-loader가 위 파일들을 처리 할 때 300kb 미만일 경우 url-loader로 처리하고 그 이상일 경우 file-laoder가 실행한다.
         },
@@ -44,6 +45,20 @@ module.exports = {
       TWO: '1+1', // 1+1의 값 2가 출력된다.
       SUMSTR: JSON.stringify('1+2'), // 1+2 문자열 자체가 출력된다.
       'api.domain': JSON.stringify('http://dev.api.domain.com'),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : '',
+      },
+      // 운영에만 키고 싶을때
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true, // 공백 제거
+              removeComments: true, // 주석 제거
+            }
+          : false,
     }),
   ],
 }
