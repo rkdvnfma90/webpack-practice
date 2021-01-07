1b0d5f406eef57bfb62e74e4b3ef9d76751b640c
DESKTOP-BGEFQLI\Administrator <rkdvnfma90@hanmail.net>
2021-01-07 13:26:42 +0900

    Add 바벨 프리셋 추가

--- my-babel-plugin.js
+++ my-babel-plugin.js
@@ -2,14 +2,13 @@ module.exports = function myBabelPlugin() {
   return {
     // 커스텀 플러그인을 만들 때에는 visitor 객체를 반환해 주어야 한다.
     visitor: {
-      Identifier(path) {
-        const name = path.node.name
+      VariableDeclaration(path) {
+        console.log('VariableDeclaration() kind:', path.node.kind) // const
 
-        // 바벨이 만든 AST 노드를 출력한다
-        console.log('Identifier() name:', name)
-
-        // 변환작업: 코드 문자열을 역순으로 변환한다
-        path.node.name = name.split('').reverse().join('')
+        // const를 var로 변경하는 부분
+        if (path.node.kind === 'const') {
+          path.node.kind = 'var'
+        }
       },
     },
   }
